238d7afc57c0d7b88967f253cfc1517dc22fc1cd
jose Lopez <vinnielo01@gmail.com>
2020-06-12 19:15:37 -0700

    testing api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,2 +1,23 @@
 const router = require("express").Router();
 const db = require("../models");
+
+router.post("/api/workouts", ({ body }, res) => {
+    db.Workout.create(body)
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+
+  router.get("/api/workouts", (req, res) => {
+    db.Workout.findOne({})
+      .sort({ date: -1 })
+      .then(dbWorkouts => {
+        res.json(dbWorkouts);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
