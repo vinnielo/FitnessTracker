6850071fff6ed5747d5e94eb7b3c4ea8432a746b
jose Lopez <vinnielo01@gmail.com>
2020-06-12 19:13:41 -0700

    updated server.js. added routes and mongoose

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+// mongoDb
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts");
+
+// routes
+
+app.use(require("./routes/api-routes.js"));
+app.use(require("./routes/html-routes.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port http://localhost:${PORT}`);
+});
