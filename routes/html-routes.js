17a543d1489b8e9427350598cbc7b08cc178ffb0
jose Lopez <vinnielo01@gmail.com>
2020-06-12 19:04:49 -0700

    added html routes

---
+++ routes/html-routes.js
@@ -0,0 +1,18 @@
+var path = require("path");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+
+  app.get("/exercise", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+  });
+
+  app.get("/stats", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+  });
+};
